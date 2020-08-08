6fba159fd23d82e26bac2de18f0d73bf82d6296a
Martin Folkers <hello@twobrain.io>
2020-08-08 18:47:25 +0200

    Initial commit

---
+++ lib/Helpers/Butler.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Fundevogel\Helpers;
+
+/**
+ * Class Butler
+ *
+ * This class contains useful helper functions, pretty much like a butler
+ * Powered by https://getkirby.com
+ *
+ * @package PHPCBIS
+ */
+
+class Butler
+{
+    /**
+     * Better alternative for implode()
+     *
+     * @param  string  $value The value to join
+     * @param  string  $separator The string to join by
+     * @param  int     $length The min length of values.
+     * @return array   An array of found values
+     */
+    public static function join($value, $separator = ', ')
+    {
+        if (is_string($value) === true) {
+            return $value;
+        }
+
+        return implode($separator, $value);
+    }
+
+
+    /**
+     * Plucks a single column from an array
+     *
+     * <code>
+     * $array[] = [
+     *   'id' => 1,
+     *   'username' => 'homer',
+     * ];
+     *
+     * $array[] = [
+     *   'id' => 2,
+     *   'username' => 'marge',
+     * ];
+     *
+     * $array[] = [
+     *   'id' => 3,
+     *   'username' => 'lisa',
+     * ];
+     *
+     * var_dump(A::pluck($array, 'username'));
+     * // result: ['homer', 'marge', 'lisa'];
+     * </code>
+     *
+     * @param array $array The source array
+     * @param string $key The key name of the column to extract
+     * @return array The result array with all values
+     *               from that column.
+     */
+    public static function pluck(array $array, string $key)
+    {
+        $output = [];
+
+        foreach ($array as $a) {
+            if (isset($a[$key]) === true) {
+                $output[] = $a[$key];
+            }
+        }
+
+        return $output;
+    }
+}
---
+++ lib/Segment.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Fundevogel;
+
+use Fundevogel\Helpers\Butler;
+
+use \SVG\Nodes\Shapes\SVGCircle;
+
+/**
+ * Class Segment
+ *
+ * Creates a donut chart segment
+ *
+ * @package tiny-phpeanuts
+ */
+class Segment
+{
+    public $color;
+    public $length;
+    public $size;
+    public $start;
+    public $thickness;
+    # TODO: Enable access to background color
+    public $backgroundColor = 'transparent';
+
+    public function __construct($color, $length, $size, $start, $thickness)
+    {
+        if ($length > 1 || $length <= 0) {
+            throw new \Exception('Please choose a value between 0 and 1');
+        }
+
+        $this->color = $color;
+        $this->length = $length;
+        $this->size = $size;
+        $this->thickness = $thickness;
+        $this->start = $start;
+    }
+
+    public function getSVGElement(): \SVG\Nodes\Shapes\SVGCircle
+    {
+        $radius = ($this->size / 2) - ($this->thickness / 2);
+        $circumference = $radius * 2 * M_PI;
+        $base = $circumference / 100;
+        $offset = $circumference - ($base * ($this->start * 100)) + ($circumference / 4);
+        $lengthOnCircle = $base * ($this->length * 100);
+
+        $circle = (new SVGCircle(
+            $this->size / 2,
+            $this->size / 2,
+            $radius
+        ))->setStyle('fill', $this->backgroundColor);
+
+        if ($this->backgroundColor === 'transparent') {
+            $circle
+                ->setStyle('fill-opacity', 0)
+                ->setStyle('stroke', $this->color)
+                ->setStyle('stroke-width', (string) $this->thickness)
+                ->setStyle('stroke-dashoffset', (string) $offset)
+                ->setStyle('stroke-dasharray', Butler::join([
+                    (string) $lengthOnCircle,
+                    (string) $circumference - $lengthOnCircle
+                ], ' '))
+            ;
+        }
+
+        return $circle;
+    }
+}
